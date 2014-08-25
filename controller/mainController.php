d068527a51663cbc5014a2d60d6f2aaa81bef4af
atiwatc <aun_cpe_mut@hotmail.com>
2014-08-25 16:49:32 +0700

    แก้ไข Controller

--- controller/mainController.php
+++ controller/mainController.php
@@ -6,11 +6,11 @@ class mainController {
 	public function __construct(){ 
 		include __DIR__.'/../Model/project_Model.php';				//ส่วนจัดการข้อมูล
 		include __DIR__.'/../View/view.php';					//ส่วนแสดงผล
-		//include __DIR__.'/../../extension/VariableModel.php';		//เก็บพวก varidate ทั่วไป
-		//include __DIR__.'/../../extension/Bootstrap.php';			//เจนเรท bootstrap เช่น datatable
-		//include __DIR__.'/../../extension/Sencha.php';				//เจนเรท sencha เช่น gridview,autocomplet
-	    //include __DIR__.'/../../extension/Jquery.php';				//เจนเรทตัว jqurey เช่น ajax,dialog,autocomplet
-		//include __DIR__.'/../../extension/basic_defaule.php';		//ตัวเจนเรททั่วไป
+		include __DIR__.'/../extension/VariableModel.php';		//เก็บพวก varidate ทั่วไป
+		include __DIR__.'/../extension/Bootstrap.php';			//เจนเรท bootstrap เช่น datatable
+		include __DIR__.'/../extension/Sencha.php';				//เจนเรท sencha เช่น gridview,autocomplet
+	    include __DIR__.'/../extension/Jquery.php';				//เจนเรทตัว jqurey เช่น ajax,dialog,autocomplet
+		include __DIR__.'/../extension/basic_defaule.php';		//ตัวเจนเรททั่วไป
 	}
 
 	public function menu(){ //ส่วนของเมนู
@@ -29,7 +29,12 @@ class mainController {
 		$view->dcodes_menu();
 		unset($view); //ทำลาย Object
 	}
-
+	
+	public function about(){ //หน้าหลัก
+		$view = new view();
+		$view->about();
+		unset($view); //ทำลาย Object
+	}
 }
 
 ?>
